829cada3b4f9a5507e9c8d0594b8ece9218cbed5
DESKTOP-C1PTSRP\zgrkp <zgrkpnr@hotmail.com>
2020-06-18 20:34:41 +0300

    added repository tests

---
+++ src/intTest/java/com/trendyol/assignment/repository/CategoryRepositoryIntTest.java
@@ -0,0 +1,49 @@
+package com.trendyol.assignment.repository;
+
+import com.trendyol.assignment.model.Category;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Optional;
+
+import static com.trendyol.assignment.utils.IntTestUtils.ANY_CATEGORY;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(locations="classpath:application.properties")
+public class CategoryRepositoryIntTest {
+
+    @Autowired
+    private CategoryRepository underTest;
+
+    @AfterEach
+    public void cleanUp() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    public void testSave() {
+        // When
+        Category category = underTest.save(ANY_CATEGORY);
+
+        // Verify
+        assertNotNull(category.getId());
+    }
+
+    @Test
+    public void testFindById() {
+        // Given
+        Category category = underTest.save(ANY_CATEGORY);
+
+        // When
+        Optional<Category> optionalCategory = underTest.findById(category.getId());
+
+        assertTrue(optionalCategory.isPresent());
+        assertEquals(category.getId(), optionalCategory.get().getId());
+    }
+}
---
+++ src/intTest/java/com/trendyol/assignment/repository/ProductRepositoryIntTest.java
@@ -0,0 +1,76 @@
+package com.trendyol.assignment.repository;
+
+import com.trendyol.assignment.model.Product;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.trendyol.assignment.utils.IntTestUtils.ANY_CATEGORY;
+import static com.trendyol.assignment.utils.IntTestUtils.ANY_PRODUCT;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(locations="classpath:application.properties")
+public class ProductRepositoryIntTest {
+
+    @Autowired
+    private ProductRepository underTest;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @BeforeAll
+    public void setUp() {
+        categoryRepository.save(ANY_CATEGORY);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    public void testSaveProduct() {
+
+        // When
+        Product product = underTest.save(ANY_PRODUCT);
+
+        // Verify
+        assertNotNull(product.getId());
+    }
+
+    @Test
+    public void testFindById() {
+
+        // Given
+        Product product = underTest.save(ANY_PRODUCT);
+
+        // When
+        Optional<Product> optionalProduct = underTest.findById(product.getId());
+
+        // Verify
+        assertTrue(optionalProduct.isPresent());
+        assertEquals(product.getId(), optionalProduct.get().getId());
+    }
+
+    @Test
+    public void testFindByCategory() {
+        // Given
+        Product product = underTest.save(ANY_PRODUCT);
+
+        // When
+        List<Product> products = underTest.findByCategory(ANY_CATEGORY);
+
+        // Verify
+        assertEquals(1, products.size());
+        assertEquals(product.getId(), products.get(0).getId());
+    }
+}
